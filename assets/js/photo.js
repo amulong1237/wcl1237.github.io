94bc5648200ba1041a59936544ce856982113855
wcl1237 <159066592322163.com>
2016-10-04 19:01:42 +0800

    Site updated: 2016-10-04 19:01:39

--- assets/js/photo.js
+++ assets/js/photo.js
@@ -19,7 +19,7 @@ var photoBox={
         for (var i = begin; i < end && i < data.length; i++) {
             li += '<li>' +
                 '<a class="img-bg" rel="example_group" href="https://github.com/wcl1237/wcl1237.github.io/blob/master/photos/' + data[i] + '?raw=true">' +
-                '<img src="https://github.com/wcl1237/wcl1237.github.io/blob/master/photos/' + data[i] + '?raw=true" /></a>' +
+                '<img  src="https://github.com/wcl1237/wcl1237.github.io/blob/master/photos/' + data[i] + '?raw=true" /></a>' +
                 '</li>'
         }
 
